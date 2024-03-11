5d310630af330768c7395f50b4e36e99498b4d6b
haol <haol666@foxmail.com>
2024-03-11 08:39:45 +0800

    增加普通对话，没有pdf

---
+++ constants.py
@@ -0,0 +1,20 @@
+from os import environ
+
+APP_VERSION = "v0.1.0 beta 25"
+APP_NAME = "AGCIS Chat CPU"
+APP_SETTINGS_FILE = "settings.yaml"
+RESULTS_DIRECTORY = "results"
+CONFIG_DIRECTORY = "configs"
+
+# LCM_DEFAULT_MODEL = "stabilityai/sd-turbo"
+# LCM_DEFAULT_MODEL_OPENVINO = "rupeshs/sd-turbo-openvino"
+# DEVICE = environ.get("DEVICE", "cpu")
+# SD_MODELS_FILE = "stable-diffusion-models.txt"
+# LCM_LORA_MODELS_FILE = "lcm-lora-models.txt"
+# OPENVINO_LCM_MODELS_FILE = "openvino-lcm-models.txt"
+# TAESD_MODEL = "madebyollin/taesd"
+# TAESDXL_MODEL = "madebyollin/taesdxl"
+# TAESD_MODEL_OPENVINO = "deinferno/taesd-openvino"
+# LCM_MODELS_FILE = "lcm-models.txt"
+# TAESDXL_MODEL_OPENVINO = "rupeshs/taesdxl-openvino"
+# LORA_DIRECTORY = "lora_models"
